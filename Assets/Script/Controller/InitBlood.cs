c9712d723d4acee848a9389a5ba3c2262a5a3b35
songzhiheng <zhiheng@89trillion.com>
2021-08-18 17:37:33 +0800

    修改注释

--- Assets/Script/Controller/InitBlood.cs
+++ Assets/Script/Controller/InitBlood.cs
@@ -9,7 +9,7 @@ using UnityEngine.UI;
 /// <summary> MyMethod is a method in the MyClass class.
 /// 初始化射手与敌人血量
 /// </summary>
-public class InitBlood : MonoBehaviour
+public class InitBlood : MonoBehaviour 
 {
     public Text enemyBlood; //敌人血量条
     public Text shootBlood; //射手血量条
